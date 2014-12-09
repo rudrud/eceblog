ccd4f8f8d760413172a08ef34847827ebdfb0ce5
rourou <rudy.leclercq.15@gmail.com>
2014-12-09 10:54:33 +0100

    suite

--- src/Ece/ArticleBundle/Controller/ArticleController.php
+++ src/Ece/ArticleBundle/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace Ece\ArticleBundle\Controller;
 
+use Ece\ArticleBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -17,6 +18,23 @@ class ArticleController extends Controller
         return array();
     }
 
+    /**
+     * @Route("/ajouter")
+     */
+    public function ajouterAction()
+    {
+        $article = new Article();
+        $article->setNom('Premier article de test');
+        $article->setDescription('Lorem ipsum ....');
+        $article->setDate(new \DateTime());
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($article);
+        $manager->flush();
+
+        exit;
+    }
+
 
     /**
      * @Route("/afficher/{id}")
@@ -24,7 +42,6 @@ class ArticleController extends Controller
      */
     public function afficherAction($id)
     {
-
 
         $name = "coucou".uniqid();
 
